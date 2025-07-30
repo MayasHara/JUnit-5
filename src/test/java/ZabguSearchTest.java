220f2f3df9fcfb0ac94932546a256fd669049ee7
MayasHara <tataurov.vlad@gmail.com>
2025-07-30 19:19:19 +0900

    Added CsvFileSource Data provider

--- src/test/java/ZabguSearchTest.java
+++ src/test/java/ZabguSearchTest.java
@@ -1,6 +1,8 @@
 import data.TestData;
 
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Tags;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
@@ -20,6 +22,20 @@ public class ZabguSearchTest extends TestData {
         $("#tabs").shouldHave(text(searchResult));
         $$(".ntitle").findBy(text(searchResult)).click();
         $("#full_text").shouldHave(text(searchResult));
+    }
+
+
+    @CsvFileSource (resources = "/Data.csv")
+    @ParameterizedTest (name = "Выполнение поиска c запросом {0} сайте https://www.zabgu.ru должно найти результат {1}")
+    @Tags({
+            @Tag("SMOKE"),
+            @Tag("REGRESS")
+    })
+    void searchGiftsForApplicationWithFile (String data, String result) {
+        $("[name='query']").setValue(data).pressEnter();
+        $("#tabs").shouldHave(text(result));
+        $$(".ntitle").findBy(text(result)).click();
+        $("#full_text").shouldHave(text(result));
     }
 
 }
